edcf6694f484830bef0233d96ecac76b8fc3e272
Jose Rios <jose@commence.studio>
2023-10-20 23:19:16 -0700

    :construction: Function to add year

--- wp-content/themes/commence/functions.php
+++ wp-content/themes/commence/functions.php
@@ -7,3 +7,8 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri() );
 }
 
+function year_shortcode() {
+	$year = date('Y');
+	return $year;
+}
+add_shortcode('year', 'year_shortcode');
