ef0158dc920c6182a0560c987682f5e59e12cbbf
Jose Rios <jose@commence.studio>
2023-11-16 11:45:55 -0800

    :construction: More

--- wp-content/themes/commence/functions.php
+++ wp-content/themes/commence/functions.php
@@ -42,53 +42,53 @@ add_filter('login_redirect', 'custom_login_redirect', 10, 3);
 
 
 
-function my_register_cpt() {
-	$labels = array(
-		'name'                  => _x( 'Courses', 'Post Type General Name', '1fix' ),
-		'singular_name'         => _x( 'Course', 'Post Type Singular Name', '1fix' ),
-		'menu_name'             => __( 'Courses', '1fix' ),
-		'name_admin_bar'        => __( 'Courses', '1fix' )
-	);
-	$args = array(
-		'label'                 => __( 'Course', '1fix' ),
-		'labels'                => $labels,
-		'hierarchical'          => true,
-		'public'                => true
-	);
-	register_post_type( 'course', $args );
-	$labels = array(
-		'name'                  => _x( 'Lesons', 'Post Type General Name', '1fix' ),
-		'singular_name'         => _x( 'Leson', 'Post Type Singular Name', '1fix' ),
-		'menu_name'             => __( 'Lesons', '1fix' ),
-		'name_admin_bar'        => __( 'Lesons', '1fix' )
-	);
-	$args = array(
-		'label'                 => __( 'Leson', '1fix' ),
-		'labels'                => $labels,
-		'hierarchical'          => false,
-		'public'                => true
-	);
-	register_post_type( 'Leson', $args );
-}
-add_action( 'init', 'my_register_cpt' );
+// function my_register_cpt() {
+// 	$labels = array(
+// 		'name'                  => _x( 'Courses', 'Post Type General Name', '1fix' ),
+// 		'singular_name'         => _x( 'Course', 'Post Type Singular Name', '1fix' ),
+// 		'menu_name'             => __( 'Courses', '1fix' ),
+// 		'name_admin_bar'        => __( 'Courses', '1fix' )
+// 	);
+// 	$args = array(
+// 		'label'                 => __( 'Course', '1fix' ),
+// 		'labels'                => $labels,
+// 		'hierarchical'          => true,
+// 		'public'                => true
+// 	);
+// 	register_post_type( 'course', $args );
+// 	$labels = array(
+// 		'name'                  => _x( 'Lesons', 'Post Type General Name', '1fix' ),
+// 		'singular_name'         => _x( 'Leson', 'Post Type Singular Name', '1fix' ),
+// 		'menu_name'             => __( 'Lesons', '1fix' ),
+// 		'name_admin_bar'        => __( 'Lesons', '1fix' )
+// 	);
+// 	$args = array(
+// 		'label'                 => __( 'Leson', '1fix' ),
+// 		'labels'                => $labels,
+// 		'hierarchical'          => false,
+// 		'public'                => true
+// 	);
+// 	register_post_type( 'Leson', $args );
+// }
+// add_action( 'init', 'my_register_cpt' );
 
-function my_add_meta_boxes() {
-	add_meta_box( 'Leson-parent', 'Course', 'Leson_attributes_meta_box', 'Leson', 'side', 'high' );
-}
-add_action( 'add_meta_boxes', 'my_add_meta_boxes' );
+// function my_add_meta_boxes() {
+// 	add_meta_box( 'Leson-parent', 'Course', 'Leson_attributes_meta_box', 'Leson', 'side', 'high' );
+// }
+// add_action( 'add_meta_boxes', 'my_add_meta_boxes' );
 
-function Leson_attributes_meta_box( $post ) {
-	$post_type_object = get_post_type_object( $post->post_type );
-	$pages = wp_dropdown_pages( array( 'post_type' => 'course', 'selected' => $post->post_parent, 'name' => 'parent_id', 'show_option_none' => __( '(no parent)' ), 'sort_column'=> 'menu_order, post_title', 'echo' => 0 ) );
-	if ( ! empty( $pages ) ) {
-		echo $pages;
-	}
-}
+// function Leson_attributes_meta_box( $post ) {
+// 	$post_type_object = get_post_type_object( $post->post_type );
+// 	$pages = wp_dropdown_pages( array( 'post_type' => 'course', 'selected' => $post->post_parent, 'name' => 'parent_id', 'show_option_none' => __( '(no parent)' ), 'sort_column'=> 'menu_order, post_title', 'echo' => 0 ) );
+// 	if ( ! empty( $pages ) ) {
+// 		echo $pages;
+// 	}
+// }
 
 function my_add_rewrite_rules() {
-	add_rewrite_tag('%leson%', '([^/]+)', 'leson=');
-	add_permastruct('leson', '/dashboard/%course%/%leson%', false);
-	add_rewrite_rule('^leson/([^/]+)/([^/]+)/?','index.php?leson=$matches[2]','top');
+	add_rewrite_tag('%lesson%', '([^/]+)', 'lesson=');
+	add_permastruct('lesson', '/dashboard/%unit%/%lesson%', false);
+	add_rewrite_rule('^lesson/([^/]+)/([^/]+)/?','index.php?lesson=$matches[2]','top');
 }
 add_action( 'init', 'my_add_rewrite_rules' );
 
